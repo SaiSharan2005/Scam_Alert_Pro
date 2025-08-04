c7dc6d01ae40c889e0bcf42bedb02d39251894ca
DUGINISAISHARAN <duginisaisharan@gmail.com>
2025-08-04 17:01:21 +0530

    added versel.json in the backend and

--- Scam_Backend/index.js
+++ Scam_Backend/index.js
@@ -67,3 +67,6 @@ app.use("/admin",profile)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+// Export the app for Vercel
+module.exports = app;
